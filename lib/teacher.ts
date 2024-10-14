e930f6505413e7b210ce2ed70df839dec97ce308
harshivg <ganwaniharshiv@gmail.com>
2024-10-14 21:03:50 +0530

    add devId

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -2,13 +2,13 @@ export const isTeacher = (userId?: string | null) => {
     if(userId === process.env.NEXT_PUBLIC_TEACHER_ID) return true;
 
     //also adding dev here
-
-    const devIds = process.env.NEXT_PUBLIC_DEV_IDS?.split(',') || [];
-    for(const dev of devIds){
-        if(userId === dev) return true;
-    }
-
+    
+    const devIds = process.env.NEXT_PUBLIC_DEV_IDS ? JSON.parse(process.env.NEXT_PUBLIC_DEV_IDS) : [];
+    
+    if(devIds.includes(userId)) return true;
+        
     return false;
 }
-
-// NEXT_PUBLIC_DEV_IDS=user_2n6WmFEUoSy2UEVoldc7eCKNpGu
+    
+    
+// NEXT_PUBLIC_DEV_IDS='["user_2n6WmFEUoSy2UEVoldc7eCKNpGu", "user_2nPvubvDW6jetjQFfSgfPXcwNmR", "user_2mqPPmky7ipFCH1IAaUHgPR0kzG"]'
