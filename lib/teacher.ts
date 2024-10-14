f14030be965457817e0ad10886c0bc738867018c
harshivg <ganwaniharshiv@gmail.com>
2024-10-14 21:13:59 +0530

    fix err

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -3,12 +3,11 @@ export const isTeacher = (userId?: string | null) => {
 
     //also adding dev here
     
-    const devIds = process.env.NEXT_PUBLIC_DEV_IDS ? JSON.parse(process.env.NEXT_PUBLIC_DEV_IDS) : [];
-    
-    if(devIds.includes(userId)) return true;
+    if(userId === process.env.NEXT_PUBLIC_DEV_IDS) return true;
         
     return false;
 }
     
     
-// NEXT_PUBLIC_DEV_IDS='["user_2n6WmFEUoSy2UEVoldc7eCKNpGu", "user_2nPvubvDW6jetjQFfSgfPXcwNmR", "user_2mqPPmky7ipFCH1IAaUHgPR0kzG"]'
+// NEXT_PUBLIC_DEV_IDS='["user_2n6WmFEUoSy2UEVoldc7eCKNpGu", "user_2nPvubvDW6jetjQFfSgfPXcwNmR", "user_2mqPPmky7ipFCH1IAaUHgPR0kzG"]
+//fix^
