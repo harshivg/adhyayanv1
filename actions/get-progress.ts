35626f66b6b329e4d750acde614c5cbc6fcc0e53
harshivg <ganwaniharshiv@gmail.com>
2024-11-17 18:53:57 +0530

    removed seed categories

--- actions/get-progress.ts
+++ actions/get-progress.ts
@@ -35,3 +35,4 @@ export const getProgress = async (
         return 0;
     }
 }
+
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -7,18 +7,7 @@ async function main() {
         // await database.category.deleteMany();
         await database.category.createMany({
             data: [
-                { name: "Web Development" },
-                { name: "Mobile Development" },
-                { name: "Data Science" },
-                { name: "Machine Learning" },
-                { name: "UI/UX" },
-                { name: "Game Development" },
-                { name: "Cybersecurity" },
-                { name: "Cloud Computing" },
-                { name: "DevOps" },
-                { name: "Digital Marketing" },
-                { name: "Business" },
-                { name: "Finance" },
+                // {name: "Web Development"},
             ]
         });
 
